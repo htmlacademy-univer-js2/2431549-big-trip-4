8476b4a3d91641ce9a0b6f2c50d9e70200551eda
r103na <rinasosiska@yandex.ru>
2024-02-16 20:25:22 +0500

    🤪

--- src/view/event-list-view.js
+++ src/view/event-list-view.js
@@ -1,10 +1,10 @@
 import { createElement } from '../render.js';
 
-const newPointTemplate = ``;
+const eventListTemplate = `<ul class="trip-events__list"></ul>`;
 
-export default class AddNewPointView {
+export default class EventListView {
   getTemplate() {
-    return newPointTemplate;
+    return eventListTemplate;
   }
 
   getElement() {
