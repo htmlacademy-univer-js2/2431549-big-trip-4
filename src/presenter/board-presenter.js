fe9dbc5049f6193d16e7e7b892daeb49d0128eb9
r103na <rinasosiska@yandex.ru>
2024-04-10 21:35:20 +0500

    Выделит показ задач в функцию

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -10,22 +10,26 @@ export default class BoardPresenter {
   #pointsModel = null;
   eventListComponent = new EventListView();
 
+  #boardPoints = [];
+
   constructor({ container, pointsModel }) {
     this.#container = container;
     this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.points = [...this.#pointsModel.points];
+    this.#boardPoints = [...this.#pointsModel.points];
 
     render(new SortView(), this.#container);
     render(this.eventListComponent, this.#container);
 
     render(new EditPointView(generatePoint()), this.eventListComponent.element);
 
-    this.points.forEach((point) => {
-      const newPoint = new PointView(point);
-      render(newPoint, this.eventListComponent.element);
-    });
+    this.#boardPoints.forEach((point) => this.#renderPoint(point));
+  }
+
+  #renderPoint(point) {
+    const newPoint = new PointView(point);
+    render(newPoint, this.eventListComponent.element);
   }
 }
