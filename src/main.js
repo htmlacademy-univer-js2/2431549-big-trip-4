7e639ecb3bcef1b2da3640585e29f30164dc9f95
r103na <rinasosiska@yandex.ru>
2024-05-25 22:32:43 +0500

    Отрефакторит фильтрацию

--- src/main.js
+++ src/main.js
@@ -3,22 +3,32 @@ import FilterView from './view/filter-view.js';
 import TripInfoView from './view/trip-info-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import { generateFilter } from './mock/filter.js';
+import { FilterType } from './const.js';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteContentElement = document.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
+
+const filterModel = new FilterModel();
+const filters = generateFilter(pointsModel.points);
+
+render(new FilterView({
+  filters,
+  currentFilterType: 'all',
+  onFilterTypeChange: () => { }
+}), siteFilterElement);
+
 const boardPresenter = new BoardPresenter(
   {
     container: siteContentElement,
     pointsModel
   });
 
-const filters = generateFilter(pointsModel.points);
 
 render(new TripInfoView(), siteHeaderElement, RenderPosition.AFTERBEGIN);
-render(new FilterView({ filters }), siteFilterElement);
 
 boardPresenter.init();
