f3945900ec9bc64c81ff5bed5059ed8436eb88e7
r103na <rinasosiska@yandex.ru>
2024-06-08 21:53:22 +0500

    Пофикшено отображение загрузки

--- src/view/loading-view.js
+++ src/view/loading-view.js
@@ -1,7 +1,6 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createLoadingTemplate = () => `<h2 class="visually-hidden">Trip events</h2>
-<p class="trip-events__msg">Loading...</p>`;
+const createLoadingTemplate = () => '<p class="trip-events__msg">Loading...</p>';
 
 export default class LoadingView extends AbstractView {
   get template() {
